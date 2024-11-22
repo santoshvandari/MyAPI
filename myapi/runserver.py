5b598ee984eef8ec9cc038fc25133c2489e116ff
Santosh Bhandari <santoshvandari1@gmail.com>
2024-11-22 12:58:51 +0545

    runserver Updated

--- myapi/runserver.py
+++ myapi/runserver.py
@@ -1,20 +1,19 @@
 import argparse
-
 import uvicorn
 
 
-def run_server(self):
-        """
-        Start the ASGI server with optional host and port arguments.
-        """
-        parser = argparse.ArgumentParser(description="MiniAPI Development Server")
-        parser.add_argument(
-            "--host", default="127.0.0.1", help="Host IP address to bind to (default: 0.0.0.0)"
-        )
-        parser.add_argument(
-            "--port", default=8000, type=int, help="Port to run the server on (default: 8000)"
-        )
-        args = parser.parse_args()
+def run_server(app):
+    """
+    Start the ASGI server with optional host and port arguments.
+    """
+    parser = argparse.ArgumentParser(description="MiniAPI Development Server Running")
+    parser.add_argument(
+        "--host", default="127.0.0.1", help="Host IP address to bind to (default: 127.0.0.1)"
+    )
+    parser.add_argument(
+        "--port", default=8080, type=int, help="Port to run the server on (default: 8080)"
+    )
+    args = parser.parse_args()
 
-        print(f"Starting server on {args.host}:{args.port}")
-        uvicorn.run(self, host=args.host, port=args.port)
+    print(f"Starting server on {args.host}:{args.port}")
+    uvicorn.run(app, host=args.host, port=args.port)
